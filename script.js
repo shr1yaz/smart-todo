c3cbb2523d8d4f3e2e1e828fc03662a98a1ca89f
Shriyaz Karki <shriyazkarki@gmail.com>
2026-09-25 23:57:37 -0600

    Added deleting task feature

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-document.querySelector('input[type="text"]');
+textInput = document.querySelector('input[type="text"]');
+textInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        const taskContainer = document.querySelector('.tasks-container');
+        taskContainer.innerHTML += `<div class="task"><input type="checkbox"></input><p>${e.target.value}</p><img src="close.png"/></div>`
+    }
+})
 
+const taskContainer = document.querySelector('.tasks-container');
+
+taskContainer.addEventListener('click', (event) => {
+    if (event.target.matches('.task img')) {
+        event.target.closest('.task').remove();
+    }
+});
